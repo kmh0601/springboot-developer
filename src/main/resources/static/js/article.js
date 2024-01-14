0de29ef389e56d73bc93349a832211ffc98c2502
kmh0601 <pongkmh@naver.com>
2024-01-14 21:19:51 +0900

    feat: blog의 게시글 생성 기능 추가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -25,16 +25,34 @@ if (modifyButton) {
         fetch(`/api/articles/${id}`, {
             method : 'PUT',
             headers: {
-                "Content-Type" : "application:json",
+                "Content-Type" : "application/json",
             },
             body: JSON.stringify({
                 title: document.getElementById('title').value,
                 content: document.getElementById('content').value
-            })
-        })
-            .then(() => {
+            }),
+        }).then(() => {
                 alert("수정이 완료되었습니다.");
                 location.replace(`/articles/${id}`);
             });
     });
+}
+
+const createButton = document.getElementById('create-btn');
+if (createButton) {
+   createButton.addEventListener("click", (event) => {
+       fetch("/api/articles", {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify({
+               title: document.getElementById('title').value,
+               content: document.getElementById('content').value,
+           }),
+       }).then(() => {
+           alert("등록 완료되었습니다.");
+           location.replace("/articles");
+       });
+   });
 }
