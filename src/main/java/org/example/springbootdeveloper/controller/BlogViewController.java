d20d69d86e32df2acaa15e502ebd1b76acd67799
kmh0601 <pongkmh@naver.com>
2024-01-14 17:22:04 +0900

    feat: blog의 글 목록에서 특정 글을 볼 수 있는 기능 구현

--- src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package org.example.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.ArticleListViewResponse;
+import org.example.springbootdeveloper.dto.ArticleViewResponse;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles",articles); // articles key에 articles data 매핑
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
