880c6eae772b0da76dfcadf25900ff12d0ebf33a
kmh0601 <pongkmh@naver.com>
2024-01-21 21:25:06 +0900

    test: test에서 비교하기 위한 토큰을 생성하기 위한 팩토리
    비교 대상 토큰과 같은 토큰을 만들 수도, 특정 클레임만 다른 토큰을 만들 수도 있는 팩토리 클래스

---
+++ src/test/java/org/example/springbootdeveloper/controller/config.jwt/JwtFactory.java
@@ -0,0 +1,47 @@
+package org.example.springbootdeveloper.controller.config.jwt;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import lombok.Getter;
+import org.example.springbootdeveloper.config.jwt.JwtProperties;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+public class JwtFactory {
+    private String subject = "test@email.com";
+    private Date issuedAt = new Date();
+    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+    private Map<String, Object> claims = Collections.emptyMap();
+
+    // 빌더 패턴을 이용해 객체를 만들 때 테스트가 필요한 데이터만 선택 -> 유효성 검증시 하나만 다르게 설정해서 실패 테스트 해볼 때 사용~
+    @Builder
+    public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
+        this.subject = subject != null ? subject : this.subject;
+        this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
+        this.expiration = expiration != null ? expiration : this.expiration;
+        this.claims = claims != null ? claims : this.claims;
+    }
+
+    public static JwtFactory withDefaultValues() {
+        return JwtFactory.builder().build();
+    }
+
+    //jjwt library를 사용해 JWT token 생성
+    public String createToken(JwtProperties jwtProperties) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssure())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+}
