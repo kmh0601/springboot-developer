ccd2dfbea8e60d5e089ad84ae85ad56d5a7b7cf1
kmh0601 <pongkmh@naver.com>
2024-02-11 21:17:31 +0900

    feat: user 인증/인가 메서드 추가

--- src/main/java/org/example/springbootdeveloper/service/BlogService.java
+++ src/main/java/org/example/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,7 @@ import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.dto.AddArticleRequest;
 import org.example.springbootdeveloper.dto.UpdateArticleRequest;
 import org.example.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,26 +20,41 @@ public class BlogService {
     public Article save(AddArticleRequest request, String userName) {
         return blogRepository.save(request.toEntity(userName));
     }
+
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
 
     public Article findById(long id) {
         return blogRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
     }
 
-    public void deleteById(long id) {
-        blogRepository.deleteById(id);
+    public void delete(long id) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
     public Article update(long id, UpdateArticleRequest request) {
         Article article = blogRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
     }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
+
 }
