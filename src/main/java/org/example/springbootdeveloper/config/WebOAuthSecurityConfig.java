99abaf6c78a8d268a0dd79679fdc17a152cb9245
kmh0601 <pongkmh@naver.com>
2024-02-11 21:18:31 +0900

    chore: import 부분 수정

--- src/main/java/org/example/springbootdeveloper/config/WebOAuthSecurityConfig.java
+++ src/main/java/org/example/springbootdeveloper/config/WebOAuthSecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.springbootdeveloper.config;
 import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.config.jwt.TokenProvider;
 import org.example.springbootdeveloper.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import org.example.springbootdeveloper.config.oauth.OAuth2SuccessHandler;
 import org.example.springbootdeveloper.config.oauth.OAuth2UserCustomService;
 import org.example.springbootdeveloper.repository.RefreshTokenRepository;
 import org.example.springbootdeveloper.service.UserService;
@@ -23,30 +24,32 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @RequiredArgsConstructor
 @Configuration
 public class WebOAuthSecurityConfig {
+
     private final OAuth2UserCustomService oAuth2UserCustomService;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
-    private UserService userService;
+    private final UserService userService;
 
     @Bean
-    public WebSecurityCustomizer configure() { // 스프링 시큐리티 기능 비활성화
+    public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
-                .requestMatchers("/img/**", "/css/**","/js/**");
+                .requestMatchers("/img/**", "/css/**", "/js/**");
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // 토큰 방식으로 인증을 하기 때문에 기존에 사용하던 폼로그인, 세션 비활성화
         http.csrf().disable()
                 .httpBasic().disable()
+                .formLogin().disable()
                 .logout().disable();
+
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // 헤더값을 확인할 커스텀 필터 추가
         http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        // 토큰 재발급 URL은 인증 없이 접근 가능하도록 설정. 나머지 API URL은 인증 필요
+
         http.authorizeRequests()
                 .requestMatchers("/api/token").permitAll()
                 .requestMatchers("/api/**").authenticated()
@@ -55,34 +58,41 @@ public class WebOAuthSecurityConfig {
         http.oauth2Login()
                 .loginPage("/login")
                 .authorizationEndpoint()
-                // Authorization 요청과 관련된 상태를 세션이 아닌 쿠키에 저장해서 쓸 수 있도록 저장소 설정
-                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieReppository())
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
                 .and()
-                .successHandler(oAuth2SuccessHandler()) // 인증 성공시 실행할 핸들러
+                .successHandler(oAuth2SuccessHandler())
                 .userInfoEndpoint()
                 .userService(oAuth2UserCustomService);
 
         http.logout()
                 .logoutSuccessUrl("/login");
 
-        // /api 로 시작하는 url인 경우 401상태 코드를 반환하도록 예외 처리
+
         http.exceptionHandling()
                 .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
                         new AntPathRequestMatcher("/api/**"));
+
+
         return http.build();
     }
 
-    @Bean public OAuth2SuccessHandler oAuth2SuccessHandler() {
-        return new OAuth2SucceessHandler(tokenProvider, refreshTokenRepository, oAuth2AuthorizationRequestBasedOnCookieRepository(),
-                userService);
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService
+        );
     }
 
-    @Bean TokenAuthenticationFilter tokenAuthenticationFilter() {
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter(tokenProvider);
     }
 
     @Bean
-    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthenticationRequestsBasedOnCookieRepository() {
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
         return new OAuth2AuthorizationRequestBasedOnCookieRepository();
     }
 
